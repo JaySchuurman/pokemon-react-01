6a8276c905cea01cf8a9323177195d63e19d163f
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-04-08 10:21:53 +0200

    Finished a pokemon display website with API

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,30 +1,72 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import React from "react";
+import "./App.css";
+import pokemonData from "./pokemonapi.json";
+import React, { useState } from "react";
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
+function App() {
+  const [pokemonList, setPokemonList] = useState(pokemonData.results);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-
-      
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-    </>
+  const filteredPokemonList = pokemonList.filter((pokemon) =>
+    pokemon.name.includes(searchTerm)
   );
-};
+
+  const showPokemon = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Error fetching Pokemon: ${response.statusText}`);
+      return;
+    }
+
+    const data = await response.json();
+    setSelectedPokemon(data);
+  };
+
+  return (
+    <div className="App">
+      <header>
+      </header>
+
+      <main>
+        <div className="search-container">
+          <input className="search-box" type="text" placeholder="Search..." 
+          value={searchTerm} 
+          onChange={event => setSearchTerm(event.target.value)}
+          />
+        </div>
+
+        {selectedPokemon && (
+          <div className="pokemon-details">
+            <h2>{selectedPokemon.name}</h2>
+            <img
+              src={selectedPokemon.sprites.front_default}
+              alt={selectedPokemon.name}
+            />
+            <p>Height: {selectedPokemon.height}</p>
+            <p>Weight: {selectedPokemon.weight}</p>
+
+            {selectedPokemon.stats.map((stat, index) => (
+              <div key={index}>
+                <p>
+                  {stat.stat.name}: {stat.base_stat}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <ul>
+          {filteredPokemonList.map((pokemon) => (
+            <li key={pokemon.id} className="pokemon-item">
+              <a href="#" onClick={() => showPokemon(pokemon.url)}>
+                {pokemon.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
 
 export default App;
